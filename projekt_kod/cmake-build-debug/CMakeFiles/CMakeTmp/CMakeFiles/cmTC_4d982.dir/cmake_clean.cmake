5ccc24228f676a4305328666c7e0dc483c444fd0
micmar007 <micmar007@utp.edu.pl>
2022-04-21 16:14:19 +0200

    Dodanie kodu aplikacji

---
+++ projekt_kod/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_4d982.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_4d982.dir/CMakeCCompilerABI.c.obj"
+  "cmTC_4d982.exe"
+  "cmTC_4d982.exe.manifest"
+  "cmTC_4d982.pdb"
+  "libcmTC_4d982.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_4d982.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
