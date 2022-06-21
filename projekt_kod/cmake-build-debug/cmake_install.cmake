dab140488fca194e0f6feabdd3547e4d18603d5f
micmar007 <micmar007@utp.edu.pl>
2022-06-21 10:28:01 +0200

    proba naprawienia błędu CI

--- projekt_kod/cmake-build-debug/cmake_install.cmake
+++ projekt_kod/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Michal/Desktop/projekt_rezerwacja_miejsc_2
+# Install script for directory: C:/Users/Michal/Desktop/system_rezerwacji_miejsc
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/projekt_rezerwacja_miejsc_2")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/system_rezerwacji_miejsc")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Michal/Desktop/projekt_rezerwacja_miejsc_2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Michal/Desktop/system_rezerwacji_miejsc/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
